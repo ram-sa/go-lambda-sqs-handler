419e8dd1c0071bbc276936711ef0ca4873681913
Ricardo Amaral <ricsm4@gmail.com>
2023-09-05 20:36:25 +0800

    + .gitignore and mocked SQS event funcion

--- mock/main.go
+++ mock/main.go
@@ -1,9 +1,16 @@
 package main
 
+// Go lambda functions run on an AL2 custom runtime. To upload it,
+// deploy the package using:
+// `GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o ./deploy/bootstrap main.go`
+// Then, zip the bootstrap into funcionName.zip and upload it.
+// Remember to correctly point the lambda's function handler to `main`
+
 import (
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -11,8 +18,8 @@ type MyEvent struct {
 	Name string `json:"name"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func HandleRequest(ctx context.Context, sqsEvent events.SQSEvent) (string, error) {
+	return fmt.Sprintf("%v", sqsEvent), nil
 }
 
 func main() {
