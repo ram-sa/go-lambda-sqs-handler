a29719b85150f85db6fb8e79cb6d9688af5bafe2
Ricardo Amaral <ricsm4@gmail.com>
2023-09-09 19:17:03 +0800

    + Added mocking logic to main file

--- mock/main.go
+++ mock/main.go
@@ -1,27 +1,84 @@
 package main
 
-// Go lambda functions run on an AL2 custom runtime. To upload it,
-// deploy the package using:
-// `GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o ./deploy/bootstrap main.go`
-// Then, zip the bootstrap into funcionName.zip and upload it.
-// Remember to correctly point the lambda's function handler to `main`
+// Go lambda functions run on an AL2 custom runtime. You can
+// deploy the package using the included script `deploy.sh`
 
 import (
-	"context"
+	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, sqsEvent events.SQSEvent) (string, error) {
-	return fmt.Sprintf("%v", sqsEvent), nil
-}
+// TODO rearrange to proper file structure
+// TODO test functions
+// TODO doc everything `https://medium.com/@helbingxxx/how-to-write-go-doc-comments-421e0ca85996`
+// TODO learn to, and then publish package
+// TODO test if pointers are faster than values as arguments
+// TODO add batching?
 
 func main() {
-	lambda.Start(HandleRequest)
+	//lambda.Start(HandleRequest)
+	simulateEvent()
+}
+
+// *** Uncomment handler when uploading lambda code *** //
+//func handleRequest(ctx context.Context, sqsEvent events.SQSEvent) error {
+//	return nil
+//}
+
+// test stuffs
+
+// Simulates an SQSEvent and some work. Alter as needed.
+// `https://github.com/aws/aws-lambda-go/blob/main/events/testdata/sqs-event.json`
+func simulateEvent() {
+	bTime := time.Now()
+	fmt.Printf("Initializing at %v\n", bTime.Format(time.TimeOnly))
+	event := events.SQSEvent{}
+	messages := []events.SQSMessage{
+		{}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+		{}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+		{}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+		{}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+		{}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+	}
+	event.Records = append(event.Records, messages...)
+	worker := WorkerImp{ExecCeiling: 6}
+
+	sTime := time.Now()
+	fmt.Printf("Starting execution at %v\n", sTime.Format(time.TimeOnly))
+	fmt.Printf("Execution Ceiling (s): %v\n", worker.ExecCeiling-1)
+	fmt.Printf("Messages: %v\n", len(messages))
+
+	b := BatchHandler{}
+	b.HandleEvent(&event, worker)
+	//m, _ := HandleEvent(event, WorkerImp{})
+	//json, _ := json.MarshalIndent(m, "", "\t")
+	//fmt.Println(string(json))
+
+	eTime := time.Now()
+	fmt.Printf("Finished execution at %v\n", eTime.Format(time.TimeOnly))
+	fmt.Printf("Duration: %v\n", eTime.Sub(sTime))
+}
+
+// Type used to test the [Worker] interface
+type WorkerImp struct {
+	// Defines the ceiling of execution time, in seconds
+	ExecCeiling int
+}
+
+// Simulates some work and generates a randomized [Report] value,
+// including an error if the value is "Failure".
+func (w WorkerImp) Work(m *events.SQSMessage) Result {
+	reports := []Status{Failure, Retry, Skip, Success}
+	r := reports[rand.Intn(len(reports))]
+	var e error = nil
+	if r == Failure {
+		e = errors.New("Errored")
+	}
+	sDur := rand.Intn(w.ExecCeiling)
+	time.Sleep(time.Second * time.Duration(sDur))
+	return Result{m, r, e}
 }
