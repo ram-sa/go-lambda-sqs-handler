5c33731e9af37a9fa84f17088ed31a1620f68faf
Ricardo Amaral <ricsm4@gmail.com>
2023-09-10 17:31:10 +0800

    + Added result handling logic

--- mock/BatchHandler.go
+++ mock/BatchHandler.go
@@ -10,19 +10,20 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
 type BatchHandler struct {
-	BackOffSettings BackOffSettings
-	Context         context.Context
-	SQSClient       sqs.Client
+	BackOffSettings     BackOffSettings
+	Context             context.Context
+	DiscardUnhandleable bool
+	FailureDlqUrl       string
+	SQSClient           sqs.Client
 }
 
 type BackOffSettings struct {
-	InitTimeoutSec int32
-	MaxTimeoutSec  int32
-	Multiplier     float64
-	RandFactor     float64
+	InitTimeoutSec, MaxTimeoutSec int32
+	Multiplier, RandFactor        float64
 }
 
 const (
@@ -32,14 +33,19 @@ const (
 	DefaultRandFactor = 0.3
 )
 
-var InvalidReceiveCountError = errors.New("Unable to parse attribute `ApproximateReceiveCount`.")
+type HandlingError struct {
+	MessageId string
+	Status    Status
+	Error     error
+}
 
-func (b *BatchHandler) HandleEvent(event *events.SQSEvent, worker Worker) (map[Status][]Result, error) {
+func (b *BatchHandler) HandleEvent(event *events.SQSEvent, worker Worker) (WorkReport, error) {
 	len := len(event.Records)
 	ch := make(chan Result, len)
-	m := make(map[Status][]Result)
+	results := make(map[Status][]Result)
 
-	// TODO use context to stop execution
+	// TODO pass context to stop execution
+	// TODO defer to failure on panic
 	for _, msg := range event.Records {
 		go func(msg *events.SQSMessage) {
 			ch <- worker.Work(msg)
@@ -50,34 +56,132 @@ func (b *BatchHandler) HandleEvent(event *events.SQSEvent, worker Worker) (map[S
 		r := <-ch
 		// TODO Call r.Validate before mapping value and wrap errors
 		// 'https://go.dev/doc/go1.20#errors'
-		m[r.Status] = append(m[r.Status], r)
+		results[r.Status] = append(results[r.Status], r)
 	}
-	// TODO handle results properly
-	b.handleResults(m)
-	return m, nil
+
+	errs := b.handleResults(results)
+	return generateReport(event, results, errs)
 }
 
-func (b *BatchHandler) handleResults(m map[Status][]Result) {
-	//err := b.handleRetries(m[Retry])
+// Process the worker's results and handles them accordingly.
+func (b *BatchHandler) handleResults(results map[Status][]Result) []HandlingError {
+	// If there are no Retries or Failures there's no reason to manually delete
+	// messages from the queue, as we can just report a success to the lambda framework.
+	if results[Retry] == nil && results[Failure] == nil {
+		return nil
+	}
 
+	var errs []HandlingError
+
+	if results[Retry] != nil {
+		errs = append(errs, b.handleRetries(results[Retry])...)
+	} else if results[Failure] != nil {
+		errs = append(errs, b.handleFailures(results[Failure])...)
+	}
+	ss := append(results[Skip], results[Success]...)
+	errs = append(errs, b.handleSkipsOrSuccesses(ss)...)
+
+	return errs
 }
 
-//func (b *BatchHandler) handleRetries(results []Result) error {
-//	if len(results) == 0 {
-//		return nil
-//	}
-//
-//	for _, r := range results {
-//		v, eVis := b.getNewVisibility(r.Message)
-//	}
-//}
+// Handles transient errors by altering a message's VisibilityTimeout with an
+// exponential backoff value.
+func (b *BatchHandler) handleRetries(results []Result) []HandlingError {
+	var errs []HandlingError
+
+	for _, r := range results {
+		v, err := b.getNewVisibility(r.Message)
+
+		if err == nil {
+			err = b.changeVisibility(r.Message, v)
+		}
+
+		if err != nil {
+			errs = append(errs, HandlingError{
+				MessageId: r.Message.MessageId,
+				Status:    r.Status,
+				Error:     err,
+			})
+			if err = b.solveUnhandleable(r.Message); err != nil {
+				errs = append(errs, HandlingError{
+					MessageId: r.Message.MessageId,
+					Status:    r.Status,
+					Error:     err,
+				})
+			}
+		}
+	}
+
+	return errs
+}
+
+// Handles unrecoverable errors by removing said messages from the queue and sending
+// them to a designated DLQ, if available.
+func (b *BatchHandler) handleFailures(results []Result) []HandlingError {
+	var errs []HandlingError
+
+	for _, r := range results {
+		if b.FailureDlqUrl != "" {
+			if err := b.sendMessage(r.Message, &b.FailureDlqUrl); err != nil {
+				errs = append(errs, HandlingError{
+					MessageId: r.Message.MessageId,
+					Status:    r.Status,
+					Error:     err,
+				})
+				if err = b.solveUnhandleable(r.Message); err != nil {
+					errs = append(errs, HandlingError{
+						MessageId: r.Message.MessageId,
+						Status:    r.Status,
+						Error:     err,
+					})
+				}
+			}
+		}
+
+		if err := b.deleteMessage(r.Message); err != nil {
+			errs = append(errs, HandlingError{
+				MessageId: r.Message.MessageId,
+				Status:    r.Status,
+				Error:     err,
+			})
+		}
+
+		/**
+		b.solveUnhandleable isn't called here since it eithers leave the message as is
+		or attempts to delete it from the queue. If the previous delete failed, there's
+		little reason to believe that attempting it again will magically solve the issue,
+		as the AWS client already has built-in fallback functionality.
+		**/
+	}
+
+	return errs
+}
+
+// Handles skipped or succesfully processed messages by removing them from the queue.
+// Called when part of the batch encountered errors.
+func (b *BatchHandler) handleSkipsOrSuccesses(results []Result) []HandlingError {
+	var errs []HandlingError
+
+	for _, r := range results {
+		if err := b.deleteMessage(r.Message); err != nil {
+			errs = append(errs, HandlingError{
+				MessageId: r.Message.MessageId,
+				Status:    r.Status,
+				Error:     err,
+			})
+		}
+	}
+
+	return errs
+}
 
 // Creates a new visibility duration for the message.
 func (b *BatchHandler) getNewVisibility(e *events.SQSMessage) (int32, error) {
 	att, ok := e.Attributes["ApproximateReceiveCount"]
 	d, err := strconv.Atoi(att)
 	if !ok || err != nil {
-		return 0, errors.Join(InvalidReceiveCountError, err)
+		errParse := errors.New("unable to parse attribute `ApproximateReceiveCount`")
+		return 0, errors.Join(errParse, err)
 	}
 	nVis := b.calculateBackoff(float64(d))
 	return nVis, nil
@@ -107,18 +211,68 @@ func (b *BatchHandler) calculateBackoff(deliveries float64) int32 {
 // Requests the original SQS queue to change the message's
 // visibility timeout to the provided value.
 func (b *BatchHandler) changeVisibility(message *events.SQSMessage, newVisibility int32) error {
-	_, err := b.SQSClient.ChangeMessageVisibility(b.Context, &sqs.ChangeMessageVisibilityInput{
-		QueueUrl:          generateQueueUrl(message.EventSourceARN),
-		ReceiptHandle:     &message.ReceiptHandle,
-		VisibilityTimeout: newVisibility,
-	})
+	url, err := generateQueueUrl(message.EventSourceARN)
+	if err == nil {
+		_, err = b.SQSClient.ChangeMessageVisibility(b.Context, &sqs.ChangeMessageVisibilityInput{
+			QueueUrl:          url,
+			ReceiptHandle:     &message.ReceiptHandle,
+			VisibilityTimeout: newVisibility,
+		})
+	}
 
 	return err
 }
 
+// Removes a message from the queue
+func (b *BatchHandler) deleteMessage(message *events.SQSMessage) error {
+	url, err := generateQueueUrl(message.EventSourceARN)
+	if err == nil {
+		_, err = b.SQSClient.DeleteMessage(b.Context, &sqs.DeleteMessageInput{
+			QueueUrl:      url,
+			ReceiptHandle: &message.ReceiptHandle,
+		})
+	}
+
+	return err
+}
+
+// Forwards a message to the designated queue
+func (b *BatchHandler) sendMessage(message *events.SQSMessage, url *string) error {
+	_, err := b.SQSClient.SendMessage(b.Context, &sqs.SendMessageInput{
+		MessageBody: &message.Body,
+		MessageAttributes: func(m map[string]events.SQSMessageAttribute) map[string]types.MessageAttributeValue {
+			convAtt := make(map[string]types.MessageAttributeValue, len(m))
+			for k, v := range m {
+				convAtt[k] = types.MessageAttributeValue{
+					DataType:         &v.DataType,
+					BinaryValue:      v.BinaryValue,
+					BinaryListValues: v.BinaryListValues,
+					StringValue:      v.StringValue,
+					StringListValues: v.StringListValues,
+				}
+			}
+			return convAtt
+		}(message.MessageAttributes),
+		QueueUrl: url,
+	})
+	return err
+}
+
+// Removes the message from the queue if [BatchHandler.DiscardUnhandleable]
+// is set.
+func (b *BatchHandler) solveUnhandleable(message *events.SQSMessage) error {
+	if !b.DiscardUnhandleable {
+		return nil
+	}
+	return b.deleteMessage(message)
+}
+
 // Builds a queue URL from its ARN
-func generateQueueUrl(queueArn string) *string {
+func generateQueueUrl(queueArn string) (*string, error) {
 	s := strings.Split(queueArn, ":")
+	if len(s) != 6 {
+		return nil, fmt.Errorf("unable to parse queue's ARN: %v", queueArn)
+	}
 	url := fmt.Sprintf("https://sqs.%v.amazonaws.com/%v/%v", s[3], s[4], s[5])
-	return &url
+	return &url, nil
 }
